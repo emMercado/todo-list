25b8315cb242a413417298fec9061cc84f0cb336
emMercado <mercadobemma55@gmail.com>
2021-05-14 20:55:03 -0300

    Agregando routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+/* import NavBar from './components/NavBar'; */
+
 
 import { ToDoList } from './components/ToDoList/ToDoList.jsx'
 
+import Jobs from './views/Jobs';
+import Companies from './views/Companies';
+import Cities from './views/Cities';
+import Countries from './views/Countries';
+import Navbar from './views/Navbar';
+import NotFoundView from './views/NotFoundView'; 
 
 
+function App() {
+  return (
+    <div className="App">
+    
+      
+      <Router>
+      <Navbar /> 
+        <Route path="/" exact component={ToDoList}></Route>
+         
+        <Route path="/jobs" exact component={Jobs} ></Route>
+        <Route path="/companies" exact component={Companies} ></Route>
+        <Route path="/cities" exact component={Cities} ></Route>
+        <Route path="/countries" exact component={Countries} ></Route>
+        <Route component={NotFoundView}></Route>
+       
+      </Router>
 
-function App(){
-  return(
-    <>
-      <ToDoList />
-    </>
-  )
+      
+    
+    </div>
+  );
 }
+
 
 
 export default App;
---
+++ src/views/Companies.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Companies extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Companies</h3>
+            </div>
+        )
+    }
+}
